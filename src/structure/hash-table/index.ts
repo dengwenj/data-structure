224273a16918091885ec619bc6e7130a2b67159e
dengwenj <927561237@qq.com>
2022-04-09 17:06:25 +0800

    哈希表的put方法

--- src/structure/hash-table/index.ts
+++ src/structure/hash-table/index.ts
@@ -9,3 +9,53 @@ export function hashFunc(str: string, max: number) {
 
   return hashCode
 }
+
+
+type Arr = [string, unknown][]
+export default class HashTable {
+  storage: Arr[]
+  count: number
+  limit: number
+
+  constructor() {
+    this.storage = []
+    this.count = 0
+    this.limit = 10
+  }
+
+  hashFunc(str: string, max: number) {
+    let hashCode = 0
+
+    for (let i = 0; i < str.length; i++) {
+      hashCode = 31 * hashCode + str.charCodeAt(i)
+    }
+
+    hashCode = hashCode % max
+
+    return hashCode
+  }
+
+  put(key: string, value: unknown) {
+    const idx = this.hashFunc(key, this.limit)
+
+    let item = this.storage[idx]
+    if (!item) {
+      item = []
+      this.storage[idx] = item
+    }
+    
+    // 判断 key 是否一样
+    let isKeySame = false
+    for (let i = 0; i < item.length; i++) {
+      if (item[i][0] === key) {
+        item[i][1] = value
+        isKeySame = true
+      }
+    }
+
+    if (!isKeySame) {
+      item.push([key, value])
+      this.count++
+    }
+  }
+}
--- src/structure/hash-table/test.ts
+++ src/structure/hash-table/test.ts
@@ -1,6 +1,16 @@
-import { hashFunc } from '.'
+import HashTable, { hashFunc } from '.'
 
-console.log(hashFunc('dwj', 10));
-console.log(hashFunc('zww', 10));
-console.log(hashFunc('hmm', 10));
-console.log(hashFunc('wxr', 10));
+// console.log(hashFunc('dwj', 10));
+// console.log(hashFunc('zww', 10));
+// console.log(hashFunc('hmm', 10));
+// console.log(hashFunc('wxr', 10));
+
+const hashTable = new HashTable()
+
+hashTable.put('name', 'dwj')
+
+hashTable.put('age', '22')
+hashTable.put('sex', '男')
+console.log(hashTable.storage)
+hashTable.put('name', 'ww')
+console.log(hashTable.storage)
